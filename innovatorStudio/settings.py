19f94a01494b3313d7a2fa627fa07805f4e8707c
harbindsingh <60515499+harbindsingh@users.noreply.github.com>
2021-07-02 13:55:37 +0530

    Update settings.py

--- innovatorStudio/settings.py
+++ innovatorStudio/settings.py
@@ -145,4 +145,4 @@ EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
