3a276b06c770a7c227e4b68ae2e6e69dd809f38d
harbindsingh <60515499+harbindsingh@users.noreply.github.com>
2021-07-02 10:21:28 +0530

    Update settings.py

--- innovatorStudio/settings.py
+++ innovatorStudio/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_DIR = [
-    os.path.join(BASE_DIR, 'eduPlatform/static'),
+    os.path.join(BASE_DIR, 'static'),
     ]
 #STATIC_ROOT=os.path.join(BASE_DIR, 'eduPltform/static')
 
