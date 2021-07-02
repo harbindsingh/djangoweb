3e39a10fd1c7df3d6454e8115010ef636b02a582
harbindsingh <60515499+harbindsingh@users.noreply.github.com>
2021-07-02 16:06:00 +0530

    Update settings.py

--- innovatorStudio/settings.py
+++ innovatorStudio/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_DIR = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'eduPltform/static'),
     ]
 #STATIC_ROOT=os.path.join(BASE_DIR, 'eduPltform/static')
 
@@ -144,4 +144,4 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'eduPltform/static')
