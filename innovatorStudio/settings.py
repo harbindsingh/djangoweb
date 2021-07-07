fd5405ad74ba10d6e52c3d2d5c2f195a510c8263
harbindsingh <60515499+harbindsingh@users.noreply.github.com>
2021-07-07 13:42:53 +0530

    Update settings.py

--- innovatorStudio/settings.py
+++ innovatorStudio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '=kd@z5p91omc(pbuqwx187^29)7dey=-lr78%vokwxz@hoiyn7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://innovatorsstudio-2pwt9.ondigitalocean.app/']
 
