1abb97df93ecfe05a49b3b72ecad700347305334
harbindsingh <60515499+harbindsingh@users.noreply.github.com>
2021-07-07 12:39:42 +0530

    Update settings.py

--- innovatorStudio/settings.py
+++ innovatorStudio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '=kd@z5p91omc(pbuqwx187^29)7dey=-lr78%vokwxz@hoiyn7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://innovatorsstudio-2pwt9.ondigitalocean.app/']
 
 
 # Application definition
