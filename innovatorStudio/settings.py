8d032d61eebc6fe8a147f5544208a7365b96dfbe
harbindsingh <60515499+harbindsingh@users.noreply.github.com>
2021-07-23 17:07:58 +0530

    Update settings.py

--- innovatorStudio/settings.py
+++ innovatorStudio/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'innovatorStudio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIRS, "db.sqlite3"),
+        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
     }
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
